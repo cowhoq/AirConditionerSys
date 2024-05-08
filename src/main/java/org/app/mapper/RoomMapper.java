3bbfae591d696354a05ab6dbca110fa8d5993048
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-05-08 15:37:45 +0800

    feat: 完成系统基本结构的搭建
    DETAILS(已完成):
    1. 系统数据库的设计, 写出了对应的 schema;
    2. 实体类以及其对应的mapper, service的编写;
    3. 配置了基础依赖, 可正确运行.

---
+++ src/main/java/org/app/mapper/RoomMapper.java
@@ -0,0 +1,12 @@
+package org.app.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.app.entity.Room;
+
+/**
+ * @author zfq
+ */
+@Mapper
+public interface RoomMapper extends BaseMapper<Room> {
+}
