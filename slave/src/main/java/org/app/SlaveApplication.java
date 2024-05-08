3988a585a7fb70d7417b9ec44145b5edc11a2e31
hbw <528190789@qq.com>
2024-05-08 23:29:08 +0800

    test

--- slave/src/main/java/org/app/SlaveApplication.java
+++ slave/src/main/java/org/app/SlaveApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlaveApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(SlaveApplication.class, args);
     }
