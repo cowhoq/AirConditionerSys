c3d9cb531418efc7974653a1846365cd9e9e261e
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-05-08 20:04:02 +0800

    refactor(MERGE-FROM: dev-zfq): 根据 `技术细节.md` 进行代码的重构
    DETAILS:
    1. 将后端细分为: master(主机), slave(从机), 并分成两个模块;
    2. master部分的代码和上一次提交的代码一样没有修改, 只是改变了文件的位置;
    3. slave部分只是一个idea自动生成的spring boot框架代码, 没有实际业务代码
    
    BREAKING CHANGE: 代码位置修改

--- slave/src/main/java/org/app/SlaveApplication.java
+++ slave/src/main/java/org/app/SlaveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AirConditionerSystemApplication {
+public class SlaveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AirConditionerSystemApplication.class, args);
+        SpringApplication.run(SlaveApplication.class, args);
     }
 
 }
