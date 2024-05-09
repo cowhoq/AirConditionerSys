98bcff0c567951cc60325f7427ce6e27f08fcea1
hbw <528190789@qq.com>
2024-05-09 23:59:06 +0800

    修改了技术细节，还有赋能scheduled

--- slave/src/main/java/org/app/SlaveApplication.java
+++ slave/src/main/java/org/app/SlaveApplication.java
@@ -2,8 +2,11 @@ package org.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SlaveApplication {
 
 
