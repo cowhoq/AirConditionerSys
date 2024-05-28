9e9fdf769d3146e6e00dac512b8b3619b1f683dc
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-05-28 18:17:31 +0800

    feat: 酒店前台界面的完成, 可以注册也可以根据 roomId 查询账单

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
     <h1>酒店前台</h1>
     <el-form @submit.prevent="registerCheckIn" class="window">
       <h2 style="text-align: center">入住登记</h2>
-      <el-form-item label="入住登记" >
+      <el-form-item label="入住登记">
         <el-input class="input" v-model="name" placeholder="输入客户姓名"></el-input>
         <el-input class="input" v-model="roomId" placeholder="输入房间号"></el-input>
         <el-button type="primary" native-type="submit">登记入住</el-button>
@@ -11,38 +11,35 @@
     </el-form>
 
     <div class="window">
-    <el-form @submit.prevent="printBill">
-      <h2 style="text-align: center">打印账单</h2>
-      <el-form-item label="输入房间号">
-        <el-input class="input" v-model="roomId" placeholder="输入房间号"></el-input>
-        <el-button type="primary" native-type="submit">获取报表</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table :data="reports" style="width: 100%">
-      <el-table-column prop="roomId" label="房间ID" align="center"></el-table-column>
-      <el-table-column prop="powerOnCount" label="总计消耗电量" align="center"></el-table-column>
-      <el-table-column label="请求" align="center">
-        <template v-slot:default="scope">
-          <el-table :data="scope.row.requests" style="width: 100%">
-            <el-table-column prop="startTime" label="Start Time"></el-table-column>
-            <el-table-column prop="endTime" label="End Time"></el-table-column>
-            <el-table-column prop="startTemp" label="Start Temperature"></el-table-column>
-            <el-table-column prop="endTemp" label="End Temperature"></el-table-column>
-            <el-table-column prop="airflow" label="Airflow"></el-table-column>
-            <el-table-column prop="cost" label="Cost"></el-table-column>
-          </el-table>
-        </template>
-      </el-table-column>
-      <el-table-column prop="总计消费" label="Total Cost" align="center"></el-table-column>
-    </el-table>
+      <el-form @submit.prevent="printBill">
+        <h2 style="text-align: center">打印账单</h2>
+        <el-form-item label="输入房间号">
+          <el-input class="input" v-model="roomId" placeholder="输入房间号"></el-input>
+          <el-button type="primary" native-type="submit">获取报表</el-button>
+        </el-form-item>
+      </el-form>
+
+      <el-table :data="reports" style="width: 100%" stripe border show-summary>
+        <el-table-column prop="roomId" label="房间ID" align="center"/>
+        <!-- TODO: 加上这个字段-->
+        <!--      <el-table-column prop="powerOnCount" label="总计消耗电量" align="center"/>-->
+        <el-table-column prop="startTime" label="Start Time"/>
+        <el-table-column prop="stopTime" label="End Time"/>
+        <el-table-column prop="startTemp" label="Start Temperature"/>
+        <el-table-column prop="stopTemp" label="End Temperature"/>
+        <el-table-column prop="fanSpeed" label="Airflow"/>
+        <el-table-column prop="totalFee" label="Cost"/>
+
+        <!--      <el-table-column prop="总计消费" label="Total Cost" align="center"/>-->
+      </el-table>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import {ref} from 'vue';
 import axiosRequest from "@/utils/axiosRequest.js";
-import { ElMessage } from "element-plus";
+import {ElMessage} from "element-plus";
 
 
 let name = ref('');
@@ -54,7 +51,7 @@ async function registerCheckIn() {
   try {
     let r = await axiosRequest({
       // 通过姓名和房间号登记入住
-      url: '/checkIn',
+      url: '/register',
       method: 'post',
       params: {
         name: name.value,
@@ -75,7 +72,7 @@ async function printBill() {
   try {
     let r = await axiosRequest({
       // 根据房间号获取对应详单
-      url: '/printBill',
+      url: '/bill',
       method: 'get',
       params: {
         roomId: roomId.value
@@ -91,7 +88,6 @@ async function printBill() {
     ElMessage.error('出错了: ' + e.message);
   }
 }
-
 </script>
 
 <style scoped>
