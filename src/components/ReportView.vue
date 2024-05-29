146cc212684b68815512032bd43a263e23c726c4
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-05-29 10:37:30 +0800

    feat: 报表

--- src/components/ReportView.vue
+++ src/components/ReportView.vue
@@ -1,28 +1,31 @@
 <template>
   <div>
     <el-form @submit.prevent="fetchReports">
-      <el-form-item label="选择报告时间">
-        <el-date-picker v-model="period" type="date" placeholder="选择日期"></el-date-picker>
-        <el-button type="primary" native-type="submit">获取报表</el-button>
+      <el-form-item label="选择报告类型">
+        <el-select v-model="period" placeholder="选择报告类型">
+          <el-option label="日报" value="DAY"/>
+          <el-option label="周报" value="WEEK"/>
+          <el-option label="月报" value="MONTH"/>
+        </el-select>
+      </el-form-item>
 
+      <el-form-item>
+        <el-button type="primary" native-type="submit">获取报表</el-button>
       </el-form-item>
     </el-form>
-    <el-table :data="reports" style="width: 100%">
-      <el-table-column prop="roomId" label="房间ID" align="center"></el-table-column>
-      <el-table-column prop="powerOnCount" label="总计消耗电量" align="center"></el-table-column>
-      <el-table-column label="请求" align="center">
-        <template v-slot:default="scope">
-          <el-table :data="scope.row.requests" style="width: 100%">
-            <el-table-column prop="startTime" label="Start Time"></el-table-column>
-            <el-table-column prop="endTime" label="End Time"></el-table-column>
-            <el-table-column prop="startTemp" label="Start Temperature"></el-table-column>
-            <el-table-column prop="endTemp" label="End Temperature"></el-table-column>
-            <el-table-column prop="airflow" label="Airflow"></el-table-column>
-            <el-table-column prop="cost" label="Cost"></el-table-column>
-          </el-table>
-        </template>
-      </el-table-column>
-      <el-table-column prop="总计消费" label="Total Cost" align="center"></el-table-column>
+
+    <el-table :data="reports" style="width: 100%" stripe border show-summary>
+      <el-table-column prop="roomId" label="房间ID" align="center"/>
+      <!-- TODO: 加上这个字段-->
+      <!--      <el-table-column prop="powerOnCount" label="总计消耗电量" align="center"/>-->
+      <el-table-column prop="startTime" label="Start Time"/>
+      <el-table-column prop="stopTime" label="End Time"/>
+      <el-table-column prop="startTemp" label="Start Temperature"/>
+      <el-table-column prop="stopTemp" label="End Temperature"/>
+      <el-table-column prop="fanSpeed" label="Airflow"/>
+      <el-table-column prop="totalFee" label="Cost"/>
+
+      <!--      <el-table-column prop="总计消费" label="Total Cost" align="center"/>-->
     </el-table>
   </div>
 </template>
@@ -37,6 +40,7 @@ let reports = ref([])
 
 async function fetchReports() {
   try {
+    console.log(period.value)
     let r = await axiosRequest({
       url: '/getTable',
       method: 'get',
