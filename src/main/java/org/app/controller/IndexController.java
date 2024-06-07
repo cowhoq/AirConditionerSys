a6d9733ecfdd54bacf00a14f6dc4475cc75d802d
zfq <137599793+Chinese-Coding@users.noreply.github.com>
2024-06-07 16:07:37 +0800

    feat: 前后端的大整合, 以后只用启动一个spring boot项目即可

--- src/main/java/org/app/controller/IndexController.java
+++ src/main/java/org/app/controller/IndexController.java
@@ -1,14 +1,55 @@
 package org.app.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin
-public class IndexController {
+public class IndexController extends BasicErrorController {
+
+    @Autowired
+    public IndexController(ErrorAttributes errorAttributes,
+                           ServerProperties serverProperties,
+                           List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, serverProperties.getError(), errorViewResolvers);
+    }
+
+    @Override
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        var status = getStatus(request);
+        // 获取 Spring Boot 默认提供的错误信息，然后添加一个自定义的错误信息
+        var model = Collections
+                .unmodifiableMap(getErrorAttributes(request,
+                        getErrorAttributeOptions(request, MediaType.TEXT_HTML)));
+        return new ModelAndView("index.html", model, status);
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        var status = getStatus(request);
+        // 获取 Spring Boot 默认提供的错误信息，然后添加一个自定义的错误信息
+        var body = getErrorAttributes(request,
+                getErrorAttributeOptions(request, MediaType.ALL));
+        return new ResponseEntity<>(body, status);
+    }
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 }
